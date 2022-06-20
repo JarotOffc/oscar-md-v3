1294aa8da63c12b068cbefd0adb62d616b4f8faa
robotew <106695308+robotew@users.noreply.github.com>
2022-06-20 23:13:42 +0700

    Update ainehi.js

--- plugins/ainehi.js
+++ plugins/ainehi.js
@@ -1,13 +1,13 @@
 let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn }) => {
-let helloaine = fs.readFileSync('./mp3/WhatsApp-Ptt-2021-07-14-at-18.12.33.opus') 
-conn.sendFile(m.chat, helloaine, '', '', m, true)
+let hallo = fs.readFileSync('./mp3/WhatsApp-Audio-2021-03-02-at-20.52.32-_2_.opus') 
+conn.sendFile(m.chat, hallo, '', '', m, true)
 //conn.sendMessage(m.chat, helloaine, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
 // await conn.sendMessage(m.chat, { audio: { url: helloaine }, mimetype: 'audio/mp4'}, m)
 }
 
-handler.customPrefix = /^(hi|hii|hiii|hi aine|hii aine|hiii aine|hy|halo|hallo|helo|hello|hy aine|halo aine|hallo aine|helo aine|hello aine)$/i
+handler.customPrefix = /^(hi|hii|hiii|hi bot|menu|.menu|hy|halo|hallo|helo|hello|hy aine|halo aine|hallo aine|helo aine|hello aine)$/i
 handler.command = new RegExp
 
 handler.limit = true
