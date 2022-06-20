630ad485bef7394d0597cb7f28d54b2a3bed3de5
robotew <106695308+robotew@users.noreply.github.com>
2022-06-20 22:42:53 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+│ •  [0858-5053-9404]
+│ •  [0858-5053-9404]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
