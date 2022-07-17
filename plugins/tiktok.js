2cb80eb5ba09814e2cea722508049d084105f153
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-07-18 00:25:22 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -30,7 +30,7 @@ handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 handler.command = /^(tiktok|tiktokdl)$/i
 handler.limit = true
-handler.group = true
+handler.group = false
 module.exports = handler
 
 /*
