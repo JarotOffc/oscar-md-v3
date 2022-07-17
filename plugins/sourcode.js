93d17360e39d4392cf4949bbaef99a33ab9966d2
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-07-17 20:11:14 +0700

    Create sourcode.js

---
+++ plugins/sourcode.js
@@ -0,0 +1,21 @@
+let handler = async (m, { conn }) => {
+	
+	// Link Script jgn di ganti ya pliss
+    // Kalo mau masukin github kamu tinggal add aja link githubnya tapi jangan di hapus sumber script!
+    // Jangan Ngeyell..
+    // Kalo ngeyel gk bakal gw up sc lgi.
+    
+    let txt = `
+Bot ini menggunakan script github
+
+Mau scriptnya chat owner wa.me/6285850539404
+Script original By Family-MD
+`
+     conn.reply(m.chat, txt, m)
+}
+handler.help = ['sourcecode']
+handler.tags = ['info']
+handler.command = /^(sc(ript(bot)?)?|sourcecode)$/i
+
+module.exports = handler
+
