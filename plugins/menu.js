4f719da61764eb0a32c18eb23857df00b28b6c8c
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-07-17 18:15:17 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -56,7 +56,7 @@ const defaultMenu = {
 ┆⬡ *Premium* : Ⓟ
 ┆⬡ *Uptime:* : %uptime (%muptime)
 ┆⬡ *Run Bot* : Okteto/heroku
-┆⬡ *Ig Owner : instagram.com/oscarbotz_
+┆⬡ *Ig Owner* : instagram.com/oscarbotz_
 ┗─────────────────⬣
 %readmore`.trimStart(),
   header: '┏━┈┈『 %category 』┈┈⬣',
