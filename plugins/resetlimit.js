2a6d0c58fa1dd297e5bb585ae4bc7344ef597326
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-04 21:17:36 +0700

    OSCAR-MD♨️

--- plugins/resetlimit.js
+++ plugins/resetlimit.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
 	let lim = !args || !args[0] ? 1000 : isNumber(args[0]) ? parseInt(args[0]) : 1000
 	lim = Math.max(1, lim)
 	list.map(([user, data], i) => (Number(data.limit = lim)))
-		conn.reply(m.chat, `*berhasil direset ${lim} / user*`, m)
+		conn.reply(m.chat, `*berhasil direset!! ${lim} / user*`, m)
 }
 handler.help = ['limit'].map(v => 'reset' + v)
 handler.tags = ['owner']
