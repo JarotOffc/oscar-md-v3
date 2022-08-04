a25ddaacb810e10fb7514424c18b1c0aa2eb2c50
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-04 21:42:48 +0700

    OSCAR-MD♨️

--- plugins/pinterest.js
+++ plugins/pinterest.js
@@ -34,6 +34,6 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 handler.help = ['pinterest <text>']
 handler.tags = ['internet']
 handler.command = /^(pinterest)$/i
-handler.limit = true
+handler.premium = true
 
 module.exports = handler
