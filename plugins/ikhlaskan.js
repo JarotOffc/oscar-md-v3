15177b1bddd677c85f1ceae82ab0f0e15ffdcc09
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-04 23:07:17 +0700

    OSCAR-MD♨️

--- plugins/ikhlaskan.js
+++ plugins/ikhlaskan.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text }) => {
   if (global.db.data.users[global.db.data.users[m.sender].pasangan].pasangan == m.sender) return conn.reply(m.chat, `Kamu telah berpacaran dengan @${global.db.data.users[m.sender].pasangan.split('@')[0]}`, m, {contextInfo: {
     mentionedJid: [global.db.data.users[m.sender].pasangan]
   }})
-  conn.reply(m.chat, `Kamu sudah mengikhlaskan @${global.db.data.users[m.sender].pasangan.split('@')[0]} karena dia tidak memberikan jawaban diterima atau ditolak`, m, {contextInfo: {
+  conn.reply(m.chat, `Kamu sudah mengikhlaskan Pasti suatu saat ketemu yang lebih baik lagi @${global.db.data.users[m.sender].pasangan.split('@')[0]} karena dia tidak memberikan jawaban diterima atau ditolak`, m, {contextInfo: {
     mentionedJid: [global.db.data.users[m.sender].pasangan]
   }})
   global.db.data.users[m.sender].pasangan = ""
