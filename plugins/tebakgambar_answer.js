6618d838db6ca0821a08df1380a8e3e0660cf84b
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-04 20:02:46 +0700

    OSCAR-MD♨️

--- plugins/tebakgambar_answer.js
+++ plugins/tebakgambar_answer.js
@@ -16,7 +16,7 @@ handler.before = async function (m) {
       clearTimeout(this.tebakgambar[id][3])
       delete this.tebakgambar[id]
     } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
-    else m.reply(`*Salah!*`)
+    else m.reply(`*Salah!!*`)
   }
   return !0
 }
