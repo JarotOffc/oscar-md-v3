7f409ac03d61fa0c392e771738fe7201adf48388
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-04 22:27:12 +0700

    OSCAR-MD♨️

--- plugins/listadmin.js
+++ plugins/listadmin.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, args, participants }) => {
         let grup = await conn.getName(m.key.remoteJid)
         let mimin = m.isGroup ? getAdmin(participants) : ''
-        let txt = `List Admin Group *${grup}*\n*Total:* ${mimin.length}\n\n`
+        let txt = `List Admin Group!! *${grup}*\n*Total:* ${mimin.length}\n\n`
         for (let min of mimin) {
                 txt += `• @${min.split('@')[0]}\n`
         }
