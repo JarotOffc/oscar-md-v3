144adbfbf5c8d63b495328dc17f127f1ae99e5e5
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-04 20:08:11 +0700

    OSCAR-MD♨️

--- plugins/tebakanime_answer.js
+++ plugins/tebakanime_answer.js
@@ -5,7 +5,7 @@ handler.before = async function (m) {
   let id = m.chat
   if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*nime/i.test(m.quoted.text)) return !0
   this.tebakanime = this.tebakanime ? this.tebakanime : {}
-  if (!(id in this.tebakanime)) return m.reply('Soal itu telah berakhir')
+  if (!(id in this.tebakanime)) return m.reply('Soal itu telah berakhir!')
   if (m.quoted.id == this.tebakanime[id][0].id) {
     let json = JSON.parse(JSON.stringify(this.tebakanime[id][1]))
     // m.reply(JSON.stringify(json, null, '\t'))
