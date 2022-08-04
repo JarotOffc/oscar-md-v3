33e5666bb69b98de22fb438f73beb5cb3cfd3062
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-04 22:45:49 +0700

    OSCAR-MD♨️

--- plugins/okick.js
+++ plugins/okick.js
@@ -10,16 +10,16 @@ let fs = require('fs')
   conn.reply(m.chat, wayy, m, { contextInfo: { mentionedJid: users }})
   for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupParticipantsUpdate(m.chat, [user], "remove")
 }
-handler.help = ['kick'].map(v => v + ' @user')
+handler.help = ['okick'].map(v => v + ' @user')
 handler.tags = ['group']
-handler.command = /^(kick|\-)$/i
-handler.owner = false
+handler.command = /^(okick|\-)$/i
+handler.owner = true
 handler.mods = false
 handler.premium = false
 handler.group = true
 handler.private = false
 
-handler.admin = true
+handler.admin = false
 handler.botAdmin = true
 
 handler.fail = null
