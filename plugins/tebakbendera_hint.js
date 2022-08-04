96be9b49eeb3de1df2d5124f5f82c247060973f8
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-04 20:04:25 +0700

    OSCAR-MD♨️

--- plugins/tebakbendera_hint.js
+++ plugins/tebakbendera_hint.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn }) => {
     let json = conn.tebakbendera[id][1]
     let ans = json.name.trim()
     let clue = ans.replace(/[AIUEOaiueo]/g, '_')
-    conn.reply(m.chat, '```' + clue + '```\nBalas soalnya, bukan pesan ini', conn.tebakbendera[id][0])
+    conn.reply(m.chat, '```' + clue + '```\nReply soalnya, bukan pesan ini', conn.tebakbendera[id][0])
 }
 handler.command = /^tekbe$/i
 handler.limit = true
