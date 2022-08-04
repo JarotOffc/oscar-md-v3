395036c959ff24c6263112901a091a4cb30b7d4a
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-04 19:03:18 +0700

    OSCAR-MD♨️

--- plugins/unbanchat.js
+++ plugins/unbanchat.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   global.db.data.chats[m.chat].isBanned = false
-  m.reply('Done!')
+  m.reply('Done!!')
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
