73c3b1a5e497948e7236973e75e6e6281ca82aba
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-04 20:14:48 +0700

    OSCAR-MD♨️

--- plugins/susunkata_hint.js
+++ plugins/susunkata_hint.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn }) => {
     let json = conn.susunkata[id][1]
     let ans = json.jawaban.trim()
     let clue = ans.replace(/[AIUEOaiueo]/g, '_')
-    conn.reply(m.chat, '```' + clue + '```\nBalas soalnya, bukan pesan ini', conn.susunkata[id][0])
+    conn.reply(m.chat, '```' + clue + '```\nReply soalnya, bukan pesan ini', conn.susunkata[id][0])
 }
 handler.command = /^suska$/i
 handler.limit = true
