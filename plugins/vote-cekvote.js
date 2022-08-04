5a088e82d6fcf8ff717cbe702f82cf0a3d9de9b7
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-04 18:53:20 +0700

    OSCAR-MD♨️

--- plugins/vote-cekvote.js
+++ plugins/vote-cekvote.js
@@ -20,7 +20,7 @@ ${devote.map(u => '@' + u.split('@')[0]).join('\n')}
 
 *${usedPrefix}hapusvote* - untuk menghapus vote
 
-_Made by aine_
+_Made by jarot_
 `.trim(), false, { contextInfo: { mentionedJid } })
 }
 handler.help = ['cekvote']
