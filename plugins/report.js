9b405d2dd0281053cc28d61df07d71cfcaa1d730
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-04 21:19:03 +0700

    OSCAR-MD♨️

--- plugins/report.js
+++ plugins/report.js
@@ -3,7 +3,7 @@
 const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async(m, { conn, text, usedPrefix, command}) => {
-    if (!text) return conn.reply(m.chat, `Silahkan masukan laporan kamu\n\nContoh: ${usedPrefix + command} Lapor pengguna mengirim foto bokep tolong di tindak.`, m)
+    if (!text) return conn.reply(m.chat, `Silahkan masukan laporan kamu\n\nContoh: ${usedPrefix + command} Lapor pengguna mengirim foto bokep tolong di tindak!!.`, m)
     if (text > 300) return conn.reply(m.chat, 'Maaf Teks Terlalu Panjang, Maksimal 300 Teks', m)
     var nomor = m.sender
     const teks1 = `*[ REPORT ]*\nNomor : wa.me/${nomor.split("@s.whatsapp.net")[0]}\nPesan : ${text}`
