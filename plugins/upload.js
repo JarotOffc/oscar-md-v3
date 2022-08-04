e0751bcb20e43a9973cc04390f18438864de90a6
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-04 18:57:44 +0700

    OSCAR-MD♨️

--- plugins/upload.js
+++ plugins/upload.js
@@ -4,7 +4,7 @@ const uploadImage = require('../lib/uploadImage')
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw 'No media found'
+  if (!mime) throw 'No media found!'
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4\/webp/.test(mime)
   let link = await (isTele ? uploadImage : uploadFile)(media)
