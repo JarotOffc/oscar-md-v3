9cb3f344acaae5c441b6a0cd7c99fcd08d5a663e
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-04 19:26:13 +0700

    OSCAR-MD♨️

--- plugins/toimg.js
+++ plugins/toimg.js
@@ -1,7 +1,7 @@
 let fs = require ('fs')
 let { webp2png } = require('../lib/webp2mp4')
 let handler = async (m, { conn, usedPrefix, command }) => {
-await m.reply('*「 WAIT 」 SEDANG PROSES...*')
+await m.reply('*「 WAIT 」 SEDANG PROSES!!...*')
     if (!m.quoted) throw `balas stiker dengan caption *${usedPrefix + command}*`
     let mime = m.quoted.mimetype || ''
     if (!/webp/.test(mime)) throw `balas stiker dengan caption *${usedPrefix + command}*`
