15b9c92c7f7879d25e563086b8570f4be9cc82f1
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-04 18:50:48 +0700

    OSCAR-MD♨️

--- plugins/vote-start.js
+++ plugins/vote-start.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text, usedPrefix }) => {
     if (id in conn.vote) {
         throw `_Masih ada vote di chat ini!_\n\n*${usedPrefix}hapusvote* - untuk menghapus vote`
     }
-    m.reply(`Vote dimulai!\n\n*${usedPrefix}upvote* - untuk ya\n*${usedPrefix}devote* - untuk tidak\n*${usedPrefix}cekvote* - untuk mengecek vote\n*${usedPrefix}hapusvote* - untuk menghapus vote`)
+    m.reply(`Vote dimulai!!\n\n*${usedPrefix}upvote* - untuk ya\n*${usedPrefix}devote* - untuk tidak\n*${usedPrefix}cekvote* - untuk mengecek vote\n*${usedPrefix}hapusvote* - untuk menghapus vote`)
     conn.vote[id] = [
         text,
         [],
