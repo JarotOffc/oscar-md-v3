dee668e7e6e84444337ced1befca0851a188af63
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-04 21:52:23 +0700

    OSCAR-MD♨️

--- plugins/mulung.js
+++ plugins/mulung.js
@@ -22,7 +22,7 @@ handler.command = /^(mulung)/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
-handler.group = true
+handler.group = false
 handler.private = false
 
 handler.admin = false
