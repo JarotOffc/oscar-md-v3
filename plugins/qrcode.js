4433c198575a1fffb51d80e43a394ba9c808a2da
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-04 21:30:19 +0700

    Update qrcode.js

--- plugins/qrcode.js
+++ plugins/qrcode.js
@@ -7,7 +7,7 @@ handler.help = ['', 'code'].map(v => 'qr' + v + ' <teks>')
 handler.tags = ['tools']
 handler.command = /^qr(code)?$/i
 
-handler.limit = true
+handler.limit = truey
 handler.fail = null
 
 module.exports = handler
