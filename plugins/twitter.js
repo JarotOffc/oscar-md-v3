215709512adfb7180206d309c984c0312662f23c
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-04 19:05:31 +0700

    OSCAR-MD♨️

--- plugins/twitter.js
+++ plugins/twitter.js
@@ -24,7 +24,7 @@ handler.help = ['twitter'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
 handler.command = /^twitter$/i
-handler.premium = true
+handler.limit = true
 handler.group = true
 
 module.exports = handler
