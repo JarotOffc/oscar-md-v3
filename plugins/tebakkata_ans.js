b96858ac32b23056da3fbe4ab4ffb68a675510f2
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-04 19:55:45 +0700

    OSCAR-MD♨️

--- plugins/tebakkata_ans.js
+++ plugins/tebakkata_ans.js
@@ -16,7 +16,7 @@ handler.before = async function (m) {
             clearTimeout(this.tebakkata[id][3])
             delete this.tebakkata[id]
         } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
-        else m.reply(`*Salah!*`)
+        else m.reply(`*Salah!!*`)
     }
     return !0
 }
