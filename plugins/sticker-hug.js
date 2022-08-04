957899cb2b5a1faea04e94ce0731bbccd9910591
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-04 20:32:02 +0700

    OSCAR-MD♨️

--- plugins/sticker-hug.js
+++ plugins/sticker-hug.js
@@ -11,7 +11,7 @@ let handler = async(m, { conn }) => {
 }
 handler.help = ['stickerhug']
 handler.tags = ['sticker']
-handler.command = /^hug|stickerhug|stikerhug$/i
+handler.command = /^hugg|stickerhug|stikerhug$/i
 handler.limit = true
 
 module.exports = handler
