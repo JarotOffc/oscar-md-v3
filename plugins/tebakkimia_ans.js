7e6fa89e5dccedb5201f8f4aa5f176b7e0e3d0af
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-04 19:53:07 +0700

    OSCAR-MD♨️

--- plugins/tebakkimia_ans.js
+++ plugins/tebakkimia_ans.js
@@ -15,7 +15,7 @@ handler.before = async function (m) {
             clearTimeout(this.tebakkimia[id][3])
             delete this.tebakkimia[id]
         } else if (similarity(m.text.toLowerCase(), json.unsur.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
-        else m.reply(`*Salah!*`)
+        else m.reply(`*Salah!!*`)
     }
     return !0
 }
