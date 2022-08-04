c43272bbcae19b6516fc815ddb3c5c9c0df3d2ec
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-04 21:40:23 +0700

    OSCAR-MD♨️

--- plugins/playstore.js
+++ plugins/playstore.js
@@ -19,7 +19,7 @@ var b = a.map((v, i) => `*${i + 1}. 「 PLAY STORE 」*\n📜 Title: _${v.name}_
 handler.help = ['playstore']
 handler.tags = ['internet']
 handler.command = /^(playstore)$/i
-handler.limit = true
+handler.premium = true
 handler.group = false
 
 module.exports = handler
