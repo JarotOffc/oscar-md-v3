6a0b03a1dd4cb756e61694e45f2782fe3ef4a745
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-04 21:07:08 +0700

    OSCAR-MD♨️

--- plugins/sendquoted.js
+++ plugins/sendquoted.js
@@ -1,7 +1,7 @@
 async function handler(m) {
     if (!m.quoted) throw 'Reply Pesannya!!'
     let q = await this.serializeM(await m.getQuotedObj())
-    if (!q.quoted) throw 'pesan yang anda reply tidak mengandung reply!'
+    if (!q.quoted) throw 'pesan yang anda reply tidak mengandung reply!!'
     await q.quoted.copyNForward(m.chat, true)
 }
 handler.help = ['q']
