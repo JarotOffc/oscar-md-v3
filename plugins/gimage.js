030de5a95581ca74a01ad6968d402645d8c7f65f
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-05 11:25:50 +0700

    OSCAR-MD♨️

--- plugins/gimage.js
+++ plugins/gimage.js
@@ -8,7 +8,7 @@ let handler  = async (m, { conn, args, text }) => {
   let { url, width, height } = pickRandom(results) || {}
   if (!url) throw '404 Not Found'
   conn.sendFile(m.chat, url, 'gimage', `
-*── 「 GOOGLE IMAGE 」 ──*
+*── 「 GOOGLE IMAGE♨️ 」 ──*
 
 ${text}
 ➸ *width*: ${width}
