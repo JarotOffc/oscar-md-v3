ec64ab3e863f56d01d91dbd5b972ebc17da5e2f8
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-05 11:29:33 +0700

    OSCAD-MD♨️

--- plugins/getplugin.js
+++ plugins/getplugin.js
@@ -1,7 +1,7 @@
 let handler = async (m, { usedPrefix, command, text }) => {
     let ar = Object.keys(plugins)
     let ar1 = ar.map(v => v.replace('.js', ''))
-    if (!text) throw `uhm.. teksnya mana?\n\ncontoh:\n${usedPrefix + command} creator`
+    if (!text) throw `uhm.. teksnya mana??\n\ncontoh:\n${usedPrefix + command} creator`
     if (!ar1.includes(text)) return m.reply(`'${text}' tidak ditemukan!\n\n${ar1.map(v => ' ' + v).join`\n`}`)
     m.reply(require('fs').readFileSync('./plugins/' + text + '.js', 'utf-8'))
 }
