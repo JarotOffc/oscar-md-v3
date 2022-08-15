ffec895f8aca6a444a7fd204c1f8a0e08eaf8bd8
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-16 00:22:26 +0700

    Create sewabot.js

---
+++ plugins/sewabot.js
@@ -0,0 +1,40 @@
+let fs = require('fs')
+let handler = async (m, { conn }) => {
+let teks = 'Sewa'
+let dana = global.dana
+let pulsa = global.pulsa
+let gopay = global.gopay
+let numberowner = global.numberowner
+let anu = `Hallo mypren 👋
+━━━〔 ıll *Sewa Bot* llı 〕━━ꕥ
+━━━━━━━━━━━━━━━━━━━━
+━━━〔 ıll *HARGA* llı 〕━━ꕥ
+⬡ *1 BULAN:* *15.000*
+⬡ *2 BULAN:* *25.000*
+⬡ *1 BULAN 2/3 GRUP:* *20.000*
+⬡ *lebih dari 3 tambah +5.000*
+┗━━━━━━━━━━━━━━━ꕥ
+┏━━━〔 ıll *PAYMENT* llı 〕━━ꕥ
+⬡ *DANA:* ${dana}
+⬡ *GOPAY:* ${gopay}
+⬡ *PULSA:* ${pulsa}
+┗━━━━━━━━━━━━━━━━━━ꕥ
+┏━━〔 ıll *RULES* llı 〕━ꕥ
+⬡ [❗] *Dana yang sudah masuk tidak bisa di kembalikan*
+┗━━━━━━━━━━ꕥ
+Jika anda berminat hubungi nomor di bawah!!
+
+⟩» *jangan lupa donasi kak* «⟨
+Terimakasih yang sudah mendonasikan untuk bot
+
+Contact person Sewa Bot:
+wa.me/${numberowner} (Owner)
+
+*Follow Instagram ku juga kak😼*`
+  conn.send3ButtonImg(m.chat, fla + teks, anu, instagram, 'Donasi', '.donasi', 'Owner', '.owner', 'Menu', '.menu', m) 
+}
+handler.help = ['sewaoscar']
+handler.tags = ['info']
+handler.command = /^(sewa|sewaoscar|sewa|sewabot)$/i
+
+module.exports = handler
