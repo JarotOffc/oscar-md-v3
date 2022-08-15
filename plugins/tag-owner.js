1b9a290db9e4b16dcfbdf4c7c1ac3e88a5398c24
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-16 05:14:00 +0700

    OSCAR-MD♨️

---
+++ plugins/tag-owner.js
@@ -0,0 +1,19 @@
+let fs = require('fs')
+
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn, text }) => {
+
+let name = m.fromMe ? conn.user : conn.contacts[m.sender]
+
+let jarot = 'https://telegra.ph/file/966adedcd3b40f50fa794.jpg'
+
+await conn.send2ButtonImg(m.chat, jarot, "*BIMLEK🐦*", 'ngapain banh tag” dia', 'OWNER', '.owner', 'KEMBALI', '.menu', m)                        
+
+}
+
+handler.customPrefix = '/@6285850539404/i
+
+handler.command = new RegExp
+
+module.exports = handler
