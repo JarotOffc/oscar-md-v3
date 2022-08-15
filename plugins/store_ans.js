c52dbd76e885eb4367c559375bcf563c74909334
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-16 05:07:56 +0700

    OSCAR-MD♨️

---
+++ plugins/store_ans.js
@@ -0,0 +1,44 @@
+/*============== Jangan di ganti! ==============*/
+
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+let fs = require('fs')
+let handler = async (m) => {
+let jarot = `
+*PILIHAN STORE*
+	`.trim()
+  let message = await prepareWAMessageMedia({ image: await (await require('node-fetch')(fotonya1)).buffer()}, { upload: conn.waUploadToServer }) 
+    const template = generateWAMessageFromContent(m.key.remoteJid, proto.Message.fromObject({
+        templateMessage: {
+            hydratedTemplate: {
+                hydratedContentText: jarot,
+                imageMessage: message.imageMessage, 
+           hydratedFooterText: '',
+           hydratedButtons: [{
+             urlButton: {
+               displayText: 'Store Jadibot',
+               url: 'https://wa.me/p/5157541794323439/6285850539404'
+             }
+
+           },
+                {
+               urlButton: {
+               displayText: 'Store Sewa Bot',
+               url: 'https://wa.me/p/5195315473908561/6285850539404'
+             }
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
+}
+
+handler.tags = ['main', 'info']
+handler.customPrefix = /^(pilihanstore|.pilihanstore)$/i
+handler.command = new RegExp
+handler.help = ['.pilihanstore']
+module.exports = handler
