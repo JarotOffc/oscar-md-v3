a7ddff04e0804c59a7b669e49c4720e871242acb
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-16 04:46:59 +0700

    OSCAR-MD♨️

---
+++ plugins/broadcastgroups.js
@@ -0,0 +1,20 @@
+let handler = async (m, { conn,isOwner, isROwner, text }) => {
+    const delay = time => new Promise(res => setTimeout(res, time))
+    let getGroups = await conn.groupFetchAllParticipating()
+    let groups = Object.entries(getGroups).slice(0).map(entry => entry[1])
+    let anu = groups.map(v => v.id)
+    let pesan = m.quoted && m.quoted.text ? m.quoted.text : text
+    if(!pesan) throw 'teksnya?'
+    m.reply(`Mengirim Broadcast Ke ${anu.length} Chat, Waktu Selesai ${anu.length * 0.5} detik`)
+    for (let i of anu) {
+    conn.send3ButtonImg(i, 'https://telegra.ph/file/b6b25d5ff74febec06fb4.jpg',pesan, wm, 'Menu', '.menu', 'Owner', '.owner', 'Donasi', '.donasi')
+    }
+  m.reply(`Sukses Mengirim Broadcast Ke ${anu.length} Group`)
+}
+handler.help = ['bcgc <teks>']
+handler.tags = ['owner']
+handler.command = /^(broadcastgc|bcgc)$/i
+
+handler.owner = true
+
+module.exports = handler
