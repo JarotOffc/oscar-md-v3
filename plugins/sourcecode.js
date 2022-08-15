731df37edbdbe83a7864f9385653d4dc1ec679af
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-16 06:31:51 +0700

    OSCAR-MD♨️

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -2,7 +2,7 @@
 
 const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
 let fs = require('fs')
-let handler = async (m) => {
+let handler = async (m, { conn }) => {
 let jarot = `
 *––––––『 SOURCE CODE 』––––––*
 	`.trim()
