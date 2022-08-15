73b288164c0e7b94f1586b5ae379a5bfa4c4be8c
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-16 06:47:03 +0700

    Update runtime.js

--- plugins/runtime.js
+++ plugins/runtime.js
@@ -35,8 +35,8 @@ module.exports = handler
 function clockString(ms) {
     let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
     let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-    let days = isNaN(ms) ? '--' : Math.floor(ms / (24 * 60 * 60 * 1000));
-    let daysms = ms % (24 * 60 * 60 * 1000);
+    let s = isNaN(ms) ? '--' : Math.floor(ms / (24 * 60 * 60 * 1000));
+    let s ms = ms % (24 * 60 * 60 * 1000);
     return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
 }
 function ucapan() {
