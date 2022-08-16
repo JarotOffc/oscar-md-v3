d1c77fcbaeab4177e532cd84568bfa05c65a0e39
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-17 04:53:15 +0700

    OSCAR-MD♨️

--- plugins/creator.js
+++ plugins/creator.js
@@ -5,7 +5,7 @@ VERSION:3.0
 N:;;;
 FN: ᴹᴿ᭄ OscarOffcོ_1ঔৣ꧂
 item.ORG: ᴹᴿ᭄ OscarOffcོ_1ঔৣ꧂
-item1.TEL;waid=6285850539404@s.whatsapp.net
+item1.TEL;waid=6285850539404:6285850539404@s.whatsapp.net
 item1.X-ABLabel:owner selalu ada 
 item2.YOUTUBE;type=INTERNET: https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ
 item2.X-ABLabel:YOUTUBE
