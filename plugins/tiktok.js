79556988d2e6e89b571977f5498fc6332edf27c7
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-17 02:55:36 +0700

    OSCAR-MD♨️

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,6 +1,6 @@
 const hxz = require("hxz-api")
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    if (!args[0]) throw `Use example ${usedPrefix}${command} https://vm.tiktok.com/ZGJAmhSrp/``
     const { author: { nickname }, video, description } = await require(args[0])
     const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
     if (!url) throw 'Can\'t download video!'
