40f8d2cf4c8e745b5a969838d93fc7af570672b4
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-17 00:05:41 +0700

    OSCAR-MD♨️

--- plugins/oscar_sound.js
+++ plugins/oscar_sound.js
@@ -1,10 +1,10 @@
 let fs = require('fs')
 let handler = async (m) => {
-let musik = fs.readFileSync('./mp3/WhatsApp-Ptt-2021-07-14-at-18.12.80.opus')
-conn.sendFile(m.chat, musik, '', '', m, true)
+let jarot = fs.readFileSync('./mp3/WhatsApp-Ptt-2021-07-14-at-18.12.80.opus')
+conn.sendFile(m.chat, jarot, '', '', m, true)
 }
 
-handler.customPrefix = /^(musik)$/i
+handler.customPrefix = /^(musik,.jarot)$/i
 handler.command = new RegExp
 
 handler.limit = true
