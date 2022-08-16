37e55c0dff0a3624161d9de8ba364cfd78fd3eac
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-17 04:30:18 +0700

    OSCAR-MD♨️

---
+++ plugins/creator_ans.js
@@ -0,0 +1,16 @@
+let fs = require('fs')
+let handler = async (m, { conn }) => {
+let teks = 'Creator'
+let dana = global.dana
+let pulsa = global.pulsa
+let gopay = global.gopay
+let numberowner = global.numberowner
+let anu = `*––––––『 INFO CREATOR 』––––––*
+`
+  conn.send2ButtonImg(m.chat, fla + teks, anu,watermark, 'Nomor creator', '.owner2', 'SosialMedia Creator', '.sosialmedia', m) 
+}
+handler.help = ['owner']
+handler.tags = ['info']
+handler.command = /^(owner|creator)$/i
+
+module.exports = handler
