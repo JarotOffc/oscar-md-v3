57beabff3434a23b845470bb7edf54f5b82cab90
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-16 23:36:13 +0700

    tes

---
+++ plugins/tiktok.js
@@ -0,0 +1,18 @@
+tiktokdlv2( 'https://api.tikmate.app).then(console.log).catch(console.error)
+console.log(await tiktokdlv2( 'https://api.tikmate.app).catch(console.error))
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    const { author: { nickname }, video, description } = await tiktokdlv2(args[0])
+    const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
+    if (!url) throw 'Can\'t download video!'
+    conn.sendFile(m.chat, url, 'tiktok.mp4', `
+*Nickname:* ${nickname}
+*Description:* ${description}
+`.trim(), m)
+}
+handler.help = ['tiktok'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+
+handler.command = /^(tik(tok)?(dl)?)$/i
+
+module.exports = handler
