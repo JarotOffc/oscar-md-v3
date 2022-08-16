6973986ac97e3aedc98ca6cd771fa02567d64a01
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-17 04:29:04 +0700

    Create creator.js

---
+++ plugins/creator.js
@@ -0,0 +1,34 @@
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+let handler = async (m, {conn}) => {
+const vcard = `BEGIN:VCARD
+VERSION:3.0
+N:;;;
+FN: ᴹᴿ᭄ OscarOffcོ_1ঔৣ꧂
+item.ORG: ᴹᴿ᭄ OscarOffcོ_1ঔৣ꧂
+item1.TEL;waid=6285850539404@s.whatsapp.net
+item1.X-ABLabel:owner selalu ada 
+item2.YOUTUBE;type=INTERNET: https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ
+item2.X-ABLabel:YOUTUBE
+item3.ADR:;;🎍AMERIKA SERIKAT;;;;
+item3.X-ABADR:ac
+item3.X-ABLabel:asal kota
+item4.URL:https//github.com/Alpiii22 
+item4.X-ABLabel:Website
+END:VCARD`
+const sentMsg  = await conn.sendMessage(
+    m.chat,
+    { 
+        contacts: { 
+            displayName: 'OWNER BOT', 
+            contacts: [{ vcard }]  
+        }
+    }
+)
+let jarot = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg'
+await conn.send3ButtonImg(m.chat, jarot, `*Hai kak @${m.sender.split('@')[0]} 👋,  Itu nomor owner gw…*`, '📮: jngn di spam kack', 'Store', '.store', 'Back', '.menu', 'Donasi', '.donasi', sentMsg)}
+handler.help = ['owner2', 'creator2']
+handler.tags = ['info']
+
+handler.command = /^(owner2|creator2)$/i
+
+module.exports = handler
