82f47b70898d207a3b8686a3f4cdd2cec62c1d9e
JAROT OFC <108053350+Alpiii22@users.noreply.github.com>
2022-08-29 05:43:32 +0700

    OSCAR-MD♨️

---
+++ plugins/donasi.js
@@ -0,0 +1,34 @@
+let fetch = require('node-fetch')
+let teks = 'Donasi'
+let handler = async (m, { conn }) => conn.send2ButtonLoc(m.chat, await (await fetch('https://telegra.ph/file/c6d47e0aa9dca2263bee9.png'+ teks)).buffer(), `
+
+*𝐇𝐚𝐥𝐨 𝐁𝐚𝐧𝐡 𝐃𝐨𝐧𝐚𝐬𝐢 𝐲𝐮𝐤 𝐁𝐢𝐚𝐫 𝐁𝐨𝐭 𝐀𝐤𝐭𝐢𝐟 𝐒𝐞𝐥𝐚𝐥𝐮👋*
+╔═══════════════════
+║ _*DONASI UNTUK*_  𝐉𝐚𝐫𝐨𝐭 ×͜×
+╠═══════════════════
+║          ❉ 〔 *𝗗𝗢𝗡𝗔𝗦𝗜* 〕 ❉
+║➸ *DANA* : 
+║➸ 085850539404
+║➸ *PULSA
+║➸ 085850539404
+║➸ *Saweria*:
+║ https://saweria.co/jarotoffc
+╰═══════════════════
+╔════════════════════
+║ *Donasi Via Ikuti Sosial media*
+╠════════════════════
+║     ❉ 〔 𝐒𝐨𝐬𝐢𝐚𝐥 𝐌𝐞𝐝𝐢𝐚 𝐉𝐚𝐫𝐨𝐭 〕 ❉
+║➸ *Instasgram*
+║ https://instagram.com/Jarotr_
+║➸ *Youtube*
+║ https://tinyurl.com/2qsejxnw
+║➸ *Github*
+║ https://github.com/Alpiii22 
+╚════════════════════
+
+`.trim(), wm, 'Owner', '.owner', 'Menu', '.menu')
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
