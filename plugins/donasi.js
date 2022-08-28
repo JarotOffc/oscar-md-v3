7946648284006fca3e474051790fabb9102c1d9b
JAROT OFC <108053350+Alpiii22@users.noreply.github.com>
2022-08-29 05:11:16 +0700

    OSCAR-MD♨️

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,14 +2,14 @@ let fetch = require('node-fetch')
 let teks = 'Donasi'
 let handler = async (m, { conn }) => conn.send2ButtonLoc(m.chat, await (await fetch('https://telegra.ph/file/c6d47e0aa9dca2263bee9.png'+ teks)).buffer(), `
 
-*Allo mypren bantu donasi yuk supaya bot aktif selalu👋*
+*𝐇𝐚𝐥𝐨 𝐁𝐚𝐧𝐡 𝐃𝐨𝐧𝐚𝐬𝐢 𝐲𝐮𝐤 𝐁𝐢𝐚𝐫 𝐁𝐨𝐭 𝐀𝐤𝐭𝐢𝐟 𝐒𝐞𝐥𝐚𝐥𝐮👋*
 ╔═══════════════════
 ║ _*DONASI UNTUK*_  𝐉𝐚𝐫𝐨𝐭 ×͜×
 ╠═══════════════════
 ║╭──❉ 〔 *𝗗𝗢𝗡𝗔𝗦𝗜* 〕 ❉──────
 ║│➸ *DANA* : 085850539404
 ║│➸ *PULSA*: 085850539404
-║│➸ *Saweria *: 'https://saweria.co/jarotoffc
+║│➸ *Saweria*:'https://saweria.co/jarotoffc
 ║╰──────────────────
 ╰═══════════════════
 
