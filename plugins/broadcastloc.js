59a3792e30717382674713e1eb6fbd96ba6a39c6
JAROT OFC <108053350+Alpiii22@users.noreply.github.com>
2022-08-28 17:31:19 +0700

    OSCAR-MD♨️

---
+++ plugins/broadcastloc.js
@@ -0,0 +1,25 @@
+let handler  = async (m, { conn, text }) => {
+  let chats = Object.keys(await conn.chats)
+  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_`, m)
+  for (let id of chats) {
+       let bcbg = 'https://telegra.ph/file/34c95be3feef20cb61dd5.jpg'
+       await conn.delay(1500)
+       await conn.send2ButtonLoc(id, bcbg,'*——————「 Broadcast 」——————*\n' + text.trim(), wm, 'Menu', '.menu', 'Owner', '.owner', m)
+     }
+  m.reply('*Broadcast selesai*')
+}
+handler.help = ['broadcastloc','bcloc'].map(v => v + ' <teks>')
+handler.tags = ['owner']
+handler.command = /^(broadcastloc|bcloc)$/i
+handler.owner = true
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
