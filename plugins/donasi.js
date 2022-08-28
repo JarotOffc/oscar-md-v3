effe97f7ca37c961cafca689198784f764809a94
JAROT OFC <108053350+Alpiii22@users.noreply.github.com>
2022-08-29 04:51:47 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,5 +1,5 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn }) => conn.send2ButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), `
+let handler = async (m, { conn }) => conn.send3ButtonImg(m.chat, await (await fetch(fla + teks)).buffer(), `
 *Allo mypren bantu donasi yuk supaya bot aktif selalu👋*
 ╔═══════════════════
 ║ _*DONASI UNTUK*_  𝐉𝐚𝐫𝐨𝐭 ×͜×
