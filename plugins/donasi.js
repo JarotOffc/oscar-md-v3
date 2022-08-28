6e57fe2673bcfcd9603d668e9b4c72b19c4f829d
JAROT OFC <108053350+Alpiii22@users.noreply.github.com>
2022-08-29 04:44:12 +0700

    Create donasi.js

---
+++ plugins/donasi.js
@@ -0,0 +1,19 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => conn.send2ButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), `
+*Allo mypren bantu donasi yuk supaya bot aktif selalu👋*
+╔═══════════════════
+║ _*DONASI UNTUK*_  𝐉𝐚𝐫𝐨𝐭 ×͜×
+╠═══════════════════
+║╭──❉ 〔 *𝗗𝗢𝗡𝗔𝗦𝗜* 〕 ❉──────
+║│➸ *DANA* : 085850539404
+║│➸ *PULSA*: 085850539404
+║│➸ *Saweria *: 'https://saweria.co/jarotoffc
+║╰──────────────────
+╰═══════════════════
+
+`.trim(), footer, 'Owner', '.owner', 'Menu', '.menu')
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
