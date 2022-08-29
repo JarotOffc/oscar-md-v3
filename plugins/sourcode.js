a1d67dd21e783385ec658744b4410f0cd1876992
JAROT OFC <108053350+Alpiii22@users.noreply.github.com>
2022-08-29 17:09:34 +0700

    OSCAR-MD♨️

---
+++ plugins/sourcode.js
@@ -0,0 +1,24 @@
+//Jangan Di ganti!! 
+// Jika ada yang Jual sc langsung Di hapus!!
+
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, 'https://telegra.ph/file/239f046a928370340401f.jpg',
+`Hi Kak @${m.sender.split('@')[0]} 
+
+◪ 📮 *SCRIPT BOT*
+│ *Script :* 
+│ ╰ https://github.com/Alpiii22/oscar-md-v3
+│ *Mampir Youtube Owner Banh:* 
+│ ╰ https://tinyurl.com/2qsejxnw
+╰──────────═┅═──────────
+`,`📍 *N o t e :* 
+• Jangan lupa mampir Youtube Owner Kak!
+• Jangan Lupa kasih star & kasih credit
+
+Official By @${'0'.split('@')[0]}
+Powered By @${`${global.owner[0]}`.split('@')[0]}`, 'Pemilik Bot', '#owner')
+handler.help = ['sourcecode']
+handler.tags = ['info']
+handler.command = /^(sourcecode|sc|scbot|script|github)$/i
+
+module.exports = handler
