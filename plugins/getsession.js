9985ab643df357a4c6b461d3c2d154d405284427
JAROT OFC <108053350+Alpiii22@users.noreply.github.com>
2022-08-30 02:53:11 +0700

    Kesalahan wkwk

--- plugins/getsession.js
+++ plugins/getsession.js
@@ -1,12 +1,12 @@
 let fs = require('fs')
 let handler = async (m, { conn, text }) => {
-    m.reply('Wait!, Proses Getting File session.data.json')
-    let sesi = await fs.readFileSync('./session.data.json')
-    return await conn.sendMessage(m.chat, { document: sesi, mimetype: 'application/json', fileName: 'session.data.json' }, { quoted: m })
+    m.reply('Wait!, Proses Getting File oscar.data.json')
+    let sesi = await fs.readFileSync('./oscar.data.json')
+    return await conn.sendMessage(m.chat, { document: sesi, mimetype: 'application/json', fileName: 'oscar.data.json' }, { quoted: m })
 }
 handler.help = ['getsessi']
 handler.tags = ['owner']
-handler.command = /^(g(et)?ses?si(on)?(data.json)?)$/i
+handler.command = /^(g(et)?oscar)?(data.json)?)$/i
 
 handler.owner = true
 
