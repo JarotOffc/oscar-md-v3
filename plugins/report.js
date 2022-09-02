6ea269bb42129eb24d85432e6dc46324845d48c3
JAROT OFC <108053350+Alpiii22@users.noreply.github.com>
2022-09-03 01:20:22 +0700

    OSCAR-MD♨️

--- plugins/report.js
+++ plugins/report.js
@@ -7,7 +7,7 @@ let handler = async(m, { conn, text, usedPrefix, command}) => {
     if (text > 300) return conn.reply(m.chat, 'Maaf Teks Terlalu Panjang, Maksimal 300 Teks', m)
     var nomor = m.sender
     const teks1 = `*[ REPORT ]*\nNomor : wa.me/${nomor.split("@s.whatsapp.net")[0]}\nPesan : ${text}`
-    conn.reply('62895330379186@s.whatsapp.net', teks1, m)
+    conn.reply('6285850539404@s.whatsapp.net', teks1, m)
     conn.reply(m.chat, '✔️ Masalah berhasil dikirimkan ke Owner', m)
 }
 handler.help = ['report <fitur>']
